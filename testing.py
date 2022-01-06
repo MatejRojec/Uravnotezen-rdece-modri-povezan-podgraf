8d7a83be65a6fa92a1b1661044ad0a965165d8a5
Vito Rozman <rozman.vito@gmali.com>
2022-01-06 20:49:26 +0100

    Testeral

--- testing.py
+++ testing.py
@@ -12,25 +12,25 @@ def prikaz(n, m, p):
     print("Stevilo vozlisc BCS: ", p)
     print('BCS podgraf: ', najvecji_p(G)[1])
 
-prikaz(10, 3, 0.456)
+#prikaz(10, 3, 0.456)
 prikaz(9, 3, 0.5)
-prikaz(12, 1, 0.7)
-prikaz(15, 4, 0.8)
-prikaz(5, 2, 0.85)
-prikaz(8, 2, 0.9)
-prikaz(3, 3, 0.976)
-prikaz(11, 2, 0.44)
-prikaz(12, 1, 0.56) 
+#prikaz(12, 1, 0.7)
+#prikaz(15, 4, 0.8)
+#prikaz(5, 2, 0.85)
+#prikaz(8, 2, 0.9)
+#prikaz(3, 3, 0.976)
+#prikaz(11, 2, 0.44)
+#prikaz(12, 1, 0.56) 
 
 
 
 def test1(n, m, p, i):
     a = []
-    while i > 0:
+    for _ in range(i):
         G = generiraj_G(n, m, p)
-        p = najvecji_p(G)[0]
-        a.append(p)
-        i -= 1
-    return (a)
+        pijv = najvecji_p(G)[0]
+        print(pijv)
+        a.append(pijv)
+    return (a, sum(a)/len(a))
 
-print(test1(10, 3, 0.5, 2))    #za i=2 še dela za 3 pa ne več :(
+#print(test1(10, 3, 0.5, 20))    #za i=2 še dela za 3 pa ne več :(
