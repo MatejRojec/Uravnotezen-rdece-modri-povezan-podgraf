22618d19486aa69cc812709d6d2de0030764548d
Vito Rozman <rozman.vito@gmali.com>
2022-01-07 23:43:38 +0100

    Naredil class in ustvaril skripto za algoritem

---
+++ ALGO_BCS/algoritem.py
@@ -0,0 +1,70 @@
+from modelBCS import *
+
+
+
+# Poisce vse vrednosti p_ijv
+def P(G):
+    B = BCS(G)
+    n = B.n
+    m = B.m # st vrsic 
+    vzorec = B.V
+    p = B.generiraj_p_ijv()
+    
+    for i in range(n): # za vsak stolpec
+        for j in range((2*n*m) + 1):
+            for v in range(len(vzorec)):
+                vz = vzorec[v] #potegnemo vzorec
+                fj = j - n*m # pretvorimo
+                vr_vz = B.vrednost_vzorca(vz, i)
+
+                if i == 0:
+                    if  vr_vz == fj:
+                        p[i][j][v] = len(vz)
+                    else:
+                        p[i][j][v] = float('-inf')
+
+                elif abs(fj) > (i + 1)*m:
+                    p[i][j][v] = float('-inf')
+
+                else:
+                    kandidati = [float('-inf')]
+                    for u in range(len(vzorec)):
+                        if abs(fj - vr_vz) >= (i+1)*m: # nam zagotovi da ne pademo ven
+                            continue
+                        else:
+                            if ujemanje(vzorec[u], vz):
+                                if povezan(vzorec[u]): # vzamemo povezanega da ne prde do tezav pri max(pijv)
+                                    st_vozlisc = len(vz) + p[i-1][j - vr_vz][u]
+                                    kandidati.append(st_vozlisc)
+                    p[i][j][v] = max(kandidati)
+    return(p)
+
+# poisce najvecji p_i0v
+def max_BCS(G):
+    B = BCS(G)
+    p = P(G)
+    r = len(p) # stolpec
+    q = len(p[0]) # st. mej za razlike j
+    s = len(p[0][0]) # st. vzorcev
+    vzorci = B.V
+    st_vozlisc = []
+    j_0 = q//2 #potegnemo j=0
+    for i in range(r):
+        for v in range(s):
+            if povezan(vzorci[v]):
+                if p[i][j_0][v] == float("-inf"):
+                    p[i][j_0][v] = 0
+                else:
+                    #print(vzorci[v], p[i][j_0][v])
+                    st_vozlisc.append(p[i][j_0][v])
+    try:
+        return(max(st_vozlisc))
+    except ValueError:
+        niz = "Graf G ne vsebuje nobenega uravnoteženega podgrafa"
+        return(niz)
+
+G1 = [[1, 1], [1, 1], [1, -1], [1, 1], [1, 1], [1, 1]] 
+G3 = [[-1, 1, -1], [1, 1, 1], [-1, 1, -1]]
+
+#print(max_BCS(G3))
+
---
+++ ALGO_BCS/modelBCS.py
@@ -0,0 +1,69 @@
+import random
+from itertools import combinations
+
+
+class BCS:
+
+    def __init__(self, G):
+        self.G = G
+        self.n = len(G) # st stolcev
+        self.m = len(G[0]) # st vrstic
+        a = [i for i in range(1, self.m)]
+        sez = [i+1 for i in range(self.m)]
+        self.V =  sum([list(map(list, combinations(sez, i))) for i in range(len(sez) + 1)], [])[1:]
+
+
+    def vrednost_vzorca(self, vzorec, i): #vrednost vzorca v i-tem stolcu grafa G
+        vsota = 0
+        stolpec = self.G[i]
+        for el in vzorec:
+            vsota += stolpec[el-1]
+        return(vsota)
+
+    def generiraj_p_ijv(self): 
+        self.p = []
+        len_v = len(self.V)
+        for i in range(self.n):
+            self.p.append([])
+            for j in range((2*self.n*self.m)+1): #
+                self.p[i].append([[] for _ in range(len_v)])
+        return(self.p)
+
+
+# ustvari graf G
+def generiraj_G(n, m, p): 
+    # n...stevilo stolcev
+    # m...stevilo vrstic
+    # p...verjetnost redecega z vrednostjo 1
+    vrednost = [1, -1]
+    G = []
+    for i in range(n):
+        G.append(random.choices(vrednost, weights = [p, 1-p], k = m))
+    return(G)
+
+# Preveri ce se vzorec v1 ujema z vzorcem v2
+def ujemanje(v1, v2): 
+    for el in v2:
+        if el in v1:
+            return True
+    return False
+
+# Preveri povezanost vzorca
+def povezan(vzorec): 
+    if len(vzorec) == 1:
+        return True
+    else:
+        i = vzorec[0]
+        for el in vzorec:
+            if i == el:
+                pass
+            else:
+                return False
+            i += 1
+        return True
+
+
+#  Poskus:
+#g = generiraj_G(4, 3, 0.5)
+#B = BCS(g)
+#print(B.n)
