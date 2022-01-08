c1dae52abf3b2bdf5e1714a6ce55716fc45b0fe6
MatejRojec <matej.4001@gmail.com>
2022-01-08 14:12:13 +0100

    vsota mora biti 0!

--- ALGO_BCS/algoritem.py
+++ ALGO_BCS/algoritem.py
@@ -5,36 +5,41 @@ from modelBCS import *
 # Poisce vse vrednosti p_ijv
 def P(G):
     B = BCS(G)
-    n = B.n
+    n = B.n # st solpcev
     m = B.m # st vrsic 
     vzorec = B.V
     p = B.generiraj_p_ijv()
     
     for i in range(n): # za vsak stolpec
-        for j in range((2*n*m) + 1):
+        for j in range((2 * n * m) + 1):
             for v in range(len(vzorec)):
                 vz = vzorec[v] #potegnemo vzorec
-                fj = j - n*m # pretvorimo
+                fj = j - n * m # pretvorimo
                 vr_vz = B.vrednost_vzorca(vz, i)
 
                 if i == 0:
                     if  vr_vz == fj:
                         p[i][j][v] = len(vz)
+                    elif fj == 0:
+                        p[i][j][v] = 0
                     else:
                         p[i][j][v] = float('-inf')
 
-                elif abs(fj) > (i + 1)*m:
+                elif abs(fj) > (i + 1) * m:
                     p[i][j][v] = float('-inf')
 
                 else:
-                    kandidati = [float('-inf')]
+                    if fj != 0:
+                        kandidati = [float('-inf')]
+                    else:
+                        kandidati = [-len(v)]    
                     for u in range(len(vzorec)):
-                        if abs(fj - vr_vz) >= (i+1)*m: # nam zagotovi da ne pademo ven
+                        if abs(fj - vr_vz) >= (i + 1)*m: # nam zagotovi da ne pademo ven
                             continue
                         else:
                             if ujemanje(vzorec[u], vz):
                                 if povezan(vzorec[u]): # vzamemo povezanega da ne prde do tezav pri max(pijv)
-                                    st_vozlisc = len(vz) + p[i-1][j - vr_vz][u]
+                                    st_vozlisc = len(vz) + p[i - 1][j - vr_vz][u]
                                     kandidati.append(st_vozlisc)
                     p[i][j][v] = max(kandidati)
     return(p)
@@ -66,5 +71,5 @@ def max_BCS(G):
 G1 = [[1, 1], [1, 1], [1, -1], [1, 1], [1, 1], [1, 1]] 
 G3 = [[-1, 1, -1], [1, 1, 1], [-1, 1, -1]]
 
-#print(max_BCS(G3))
+print(max_BCS(G3))
 
