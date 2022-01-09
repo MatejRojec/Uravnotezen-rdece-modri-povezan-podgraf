f8394dfd7f678d26757df3508003aa3c77dc6a02
Vito Rozman <rozman.vito@gmali.com>
2022-01-09 15:19:48 +0100

    Dodali vracanje vozlisc

--- ALGO_BCS/algoritem.py
+++ ALGO_BCS/algoritem.py
@@ -8,7 +8,7 @@ def P(G):
     m = B.m # st vrsic 
     vzorec = B.VZ
     p = B.generiraj_p_ijv()
-    #V = B.generiraj_V()
+    V = B.generiraj_V()
     for i in range(n): # za vsak stolpec
         for j in range((2 * n * m) + 1):
             for v in range(len(vzorec)):
@@ -19,20 +19,20 @@ def P(G):
                 if i == 0:
                     if  vr_vz == fj:
                         p[i][j][v] = dolzina_v
-                        #V[i][j][v] = vz
+                        V[i][j][v] = vz
                     elif fj == 0:
                         p[i][j][v] = 0
-                        #V[i][j][v] = None
+                        V[i][j][v] = None
                     else:
                         p[i][j][v] = float('-inf')
-                        #V[i][j][v] = None
+                        V[i][j][v] = None
 
                 elif abs(fj) > (i + 1) * m:
                     p[i][j][v] = float('-inf')
-                    #V[i][j][v] = None
+                    V[i][j][v] = None
 
                 else:
-                    #kandidati_vozlisc = [None]
+                    kandidati_vozlisc = [None]
                     if fj != 0:
                         kandidati = [float('-inf')]
                     else:
@@ -54,46 +54,47 @@ def P(G):
                                     kandidati.append(st_vozlisc)
                             #if ujemanje(vzorec[u], vz):
                                 # if povezan(vzorec[u]): # vzamemo povezanega da ne prde do tezav pri max(pijv)
-                                #kandidati_vozlisc.append([V[i-1][j - vr_vz][u], vz])
+                                    kandidati_vozlisc.append([V[i-1][j - vr_vz][u], vz])
                     max_kandidat = max(kandidati)
                     indeks = kandidati.index(max_kandidat)
                     p[i][j][v] = max_kandidat
-                    #V[i][j][v] = kandidati_vozlisc[indeks]
-    return(p)
+                    V[i][j][v] = kandidati_vozlisc[indeks]
+    return(p, V)
 
 # poisce najvecji p_i0v
 def max_BCS(G):
     B = BCS(G)
-    p = P(G)
+    p, V = P(G)
     r = len(p) # stolpec
     q = len(p[0]) # st. mej za razlike j
     s = len(p[0][0]) # st. vzorcev
     vzorci = B.VZ
     st_vozlisc = []
-    #sez_vozlisc = []
+    sez_vozlisc = []
     j_0 = q//2 #potegnemo j=0
     for i in range(r):
         for v in range(s):
             if len(vzorci[v]) == 1:
             #if povezan(vzorci[v]):
                 st_vozlisc.append(p[i][j_0][v])
-                #sez_vozlisc.append(V[i][j_0][v])
+                sez_vozlisc.append(V[i][j_0][v])
     try:
         maxi = max(st_vozlisc)
         indeks = st_vozlisc.index(maxi)
-        return(maxi)
+        return(maxi, sez_vozlisc[indeks])
     except ValueError:
         niz = "Graf G ne vsebuje nobenega uravnoteženega podgrafa"
-        return(niz)
+        return(niz, "!")
 
-G1 = [[-1, 1], [1, 1], [1, -1], [1, 1], [1, -1], [1, 1]] 
-G3 = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+G1 = [[1, 1], [1, 1], [1, -1], [1, 1], [1, -1], [1, 1]] 
+G3 = [[1, 1, 1], [1, 1, 1], [-1, -1, -1]]
 
 G4 = [[-1,1,1,1],[1,1,1,1],[-1,1,1,1],[1,1,1,1],[-1,1,-1,1]]
 
+G5 = generiraj_G(10, 2, 0.5)
+
+G6 = [[1], [1], [1], [1], [1], [-1], [-1], [1], [1], [-1], [1], [1]] 
 #print(P(G1))
 
+#print(P(G1))
 print(max_BCS(G4))
-
-G5 = generiraj_G(50, 4, 0.5)
-print(max_BCS(G5))
