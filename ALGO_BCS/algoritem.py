ff0502997695bdaeba81257d77f311d5ed68cf8b
Vito Rozman <rozman.vito@gmali.com>
2022-01-09 23:36:34 +0100

    Koncni algoritem

--- ALGO_BCS/algoritem.py
+++ ALGO_BCS/algoritem.py
@@ -1,7 +1,6 @@
 from modelBCS import *
 
-# Poisce vse vrednosti p_ijv
-
+# Poisce vse vrednosti p_ijv in vozlisca V_ijv
 def P(G):
     B = BCS(G)
     n = B.n # st solpcev
@@ -19,23 +18,24 @@ def P(G):
                 if i == 0:
                     if any(len(d) > 1 for d in vzorec[v]):
                         p[i][j][v] = float('-inf')
+                        V[i][j][v] = [[]]
                     else:
                         if  vr_vz == fj:
                             p[i][j][v] = dolzina_v
                             V[i][j][v] = vz
                         elif fj == 0:
                             p[i][j][v] = 0
-                            V[i][j][v] = None
+                            V[i][j][v] = [[]]
                         else:
                             p[i][j][v] = float('-inf')
-                            V[i][j][v] = None
+                            V[i][j][v] = [[]]
 
                 elif abs(fj) > (i + 1) * m:
                     p[i][j][v] = float('-inf')
-                    V[i][j][v] = None
+                    V[i][j][v] = [[]]
 
                 else:
-                    kandidati_vozlisc = [None]
+                    kandidati_vozlisc = [[[]]]
                     if fj != 0:
                         kandidati = [float('-inf')]
                     else:
@@ -44,20 +44,16 @@ def P(G):
                         if abs(fj - vr_vz) >= (i + 1)*m: # nam zagotovi da ne pademo ven
                             continue
                         else:
-
                             du = [set(sum(d, [])) for d in vzorec[u]] # deli u
                             vv = set(sum(sum(vzorec[v], []),[])) # množica vozlišč v
                             pogoj1 = all(any(x in vv for x in d) for d in du)
                             pogoj2 = all(any(all(any(x in c for x in e) for c in d) for e in du) for d in vzorec[v] if len(d) > 1)
                             if pogoj1 and pogoj2:
                                 if any(len(d) > 1 for d in vzorec[v]) and p[i - 1][j - vr_vz][u] == 0:
-                                #if vzorec[v] in [ [[[1], [3]]], [[[1], [4]]], [[[2], [4]]], [[[1, 2], [4]]], [[[1], [3, 4]]]] and fj == 0:
                                     continue
                                 else:
                                     st_vozlisc = dolzina_v + p[i - 1][j - vr_vz][u]
                                     kandidati.append(st_vozlisc)
-                            #if ujemanje(vzorec[u], vz):
-                                # if povezan(vzorec[u]): # vzamemo povezanega da ne prde do tezav pri max(pijv)
                                     kandidati_vozlisc.append([V[i-1][j - vr_vz][u], vz])
                     max_kandidat = max(kandidati)
                     indeks = kandidati.index(max_kandidat)
@@ -76,29 +72,15 @@ def max_BCS(G):
     st_vozlisc = []
     sez_vozlisc = []
     j_0 = q//2 #potegnemo j=0
+    l = []
     for i in range(r):
         for v in range(s):
             if len(vzorci[v]) == 1:
-            #if povezan(vzorci[v]):
                 st_vozlisc.append(p[i][j_0][v])
                 sez_vozlisc.append(V[i][j_0][v])
-    try:
-        maxi = max(st_vozlisc)
-        indeks = st_vozlisc.index(maxi)
-        return(maxi, sez_vozlisc[indeks])
-    except ValueError:
-        niz = "Graf G ne vsebuje nobenega uravnoteženega podgrafa"
-        return(niz, "!")
-
-G1 = [[1, 1], [1, 1], [1, -1], [1, 1], [1, -1], [1, 1]] 
-G3 = [[1, 1, 1], [1, 1, 1], [-1, -1, -1]]
-
-G4 = [[-1,1,1,1],[1,1,1,1],[-1,1,1,1],[1,1,1,1],[-1,1,-1,1]]
-
-G5 = generiraj_G(10, 2, 0.5)
-
-G6 = [[1], [1], [1], [1], [1], [-1], [-1], [1], [1], [-1], [1], [1]] 
-#print(P(G1))
-G7 = generiraj_G(20, 1, 0.5)
-#print(P(G1))
-#print(max_BCS(G7))
+                l.append(i)
+    maxi = max(st_vozlisc)
+    indeks = st_vozlisc.index(maxi)
+    podgraf = precisti_sez(maxi, sez_vozlisc[indeks], l[indeks], B.n - 1)
+    return(maxi, podgraf)
+    
--- ALGO_BCS/modelBCS.py
+++ ALGO_BCS/modelBCS.py
@@ -8,38 +8,38 @@ VZORCI = {
     2 :  [ 
         [[[1]]], 
         [[[2]]], 
-        [[[1, 2]]]],
+        [[[1,2]]]],
     3 : [
         [[[1]]], 
         [[[2]]], 
         [[[3]]], 
-        [[[1, 2]]], 
-        [[[1], [3]]], 
-        [[[1]], [[3]]], 
-        [[[2, 3]]],
-        [[[1, 2, 3]]]
+        [[[1,2]]], 
+        [[[1],[3]]], 
+        [[[1]],[[3]]], 
+        [[[2,3]]],
+        [[[1,2,3]]]
     ],
     4 : [
-        [[[1]]] ,
-        [[[2]]] ,
-        [[[3]]] ,
-        [[[4]]] ,
-        [[[1, 2]]],
-        [[[1], [3]]],
-        [[[1]], [[3]]],
-        [[[1], [4]]],
-        [[[1]], [[4]]],
-        [[[2, 3]]],
-        [[[2], [4]]],
-        [[[2]], [[4]]],
-        [[[3, 4]]],
-        [[[1, 2, 3]]],
-        [[[1, 2], [4]]],
-        [[[1, 2]], [[4]]],
-        [[[1], [3, 4]]],
-        [[[1]], [[3, 4]]],
-        [[[2, 3, 4]]],
-        [[[1, 2, 3, 4]]]
+        [[[1]]],
+        [[[2]]],
+        [[[3]]],
+        [[[4]]],
+        [[[1,2]]],
+        [[[1],[3]]],
+        [[[1]],[[3]]],
+        [[[1],[4]]],
+        [[[1]],[[4]]],
+        [[[2,3]]],
+        [[[2],[4]]],
+        [[[2]],[[4]]],
+        [[[3,4]]],
+        [[[1,2,3]]],
+        [[[1,2],[4]]],
+        [[[1,2]],[[4]]],
+        [[[1],[3,4]]],
+        [[[1]],[[3,4]]],
+        [[[2,3,4]]],
+        [[[1,2,3,4]]]
     ]
 }
 
@@ -50,11 +50,7 @@ class BCS:
         self.G = G
         self.n = len(G) # st stolcev
         self.m = len(G[0]) # st vrstic
-        a = [i for i in range(1, self.m)]
-        sez = [i+1 for i in range(self.m)]
-        # VZ = vsi mozni vzorci
         self.VZ = VZORCI[self.m]
-        #self.VZ =  sum([list(map(list, combinations(sez, i))) for i in range(len(sez) + 1)], [])[1:]
     
     def vrednost_vzorca(self, vzorec, i): #vrednost vzorca v i-tem stolcu grafa G
         # to vrne stali vzorec
@@ -97,31 +93,38 @@ def generiraj_G(n, m, p):
     for i in range(n):
         G.append(random.choices(vrednost, weights = [p, 1-p], k = m))
     return(G)
-# Preveri ce se vzorec v1 ujema z vzorcem v2
-def ujemanje(v1, v2): 
-    for el in v2:
-        if el in v1:
-            return True
-    return False
-# Preveri povezanost vzorca
-def povezan(vzorec): 
-    if len(vzorec) == 1:
-        return True
-    else:
-        i = vzorec[0]
-        for el in vzorec:
-            if i == el:
-                pass
-            else:
-                return False
-            i += 1
-        return True
 
+# pocisti seznam 
+def precisti_sez(p, sez, zacetek, dolzina):
+    if p == 0:
+        return([])
 
-#  Poskus:
-#G3 = [[-1, 1, -1], [-1, -1, 1], [-1, 1, -1]]
-#g = generiraj_G(4, 3, 0.5)
-#B = BCS(G3)
-
-
-#print(B.vrednost_vzorca(el, 1))
+    sez = str(sez)
+    sez = sez.replace(" 2", "2").replace(" 3", "3").replace(" 4", "4")
+    niz = ""
+    for i in sez:
+        if i in "[]":
+            pass
+        else:
+            niz += i
+    s = niz.split(" ")
+    l = []
+    for el in s:
+        if el != ",":
+            if el[-1] == ",":
+                el = el[:-1]
+            l.append(el)
+    listic = []
+    for el in l:
+        v = el.split(",")
+        s = []
+        for i in v:
+            s.append(int(i))
+        listic.append(s)
+    j = len(listic)-1
+    for _ in range(zacetek-j):
+        listic = [[]] + listic
+    if dolzina-zacetek != 0:
+        for _ in range(dolzina-zacetek):
+            listic.append([] )
+    return(listic)
